65aa4ff68cc5e1aba050011fcc2d5602595b5eb2
Sankaranarayanan <ssankarmca37@gmail.com>
2021-03-07 17:19:28 +0530

    Cleanup Artifacts

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ WORKSPACE=$( pwd )
 # shellcheck source=/dev/null
 . "${WORKSPACE}/devops/scripts/.env"
 
+#Clean Up Previous Build Artifacts
+rm -rf artifacts
+
 #Compiling Affected Lambdas
 ./devops/scripts/build.sh "${WORKSPACE}"
 
